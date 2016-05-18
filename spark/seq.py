198b0a5c8b058ef3c61a3b8103b55527241c917d
Maria Svensson <mariasvenson@live.com>
2016-05-18 18:29:34 +0200

    save all kmers in list

--- spark/seq.py
+++ spark/seq.py
@@ -5,11 +5,14 @@ import pysam
 bamUrl = "http://130.238.29.253:8080/swift/v1/1000-genomes-dataset/HG00096.chrom20.ILLUMINA.bwa.GBR.low_coverage.20120522.bam"
 with pysam.AlignmentFile(bamUrl,"rb") as samfile:
         kmer = 10
+        kmer_list = []
         for r in samfile.fetch(until_eof=True):
                 if r.is_unmapped:
                         test = r.query_alignment_sequence
                         for x in range(len(test)+1 - kmer):
                                 kmers = test[x:x+kmer]
-                                with open("/home/ubuntu/output/out_seq.txt", "a") as f:
-                                        f.write(str(kmers) + "\n")
+                                kmer_list.append(kmers)
+                                #with open("/home/ubuntu/output/out_seq.txt", "a") as f:
+                                #       f.write(str(kmers) + "\n")
+                        print  len(kmer_list)
         samfile.close()
