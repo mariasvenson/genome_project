8643a2063a691a78c2db638837b6ea992216b2c9
Ubuntu <ubuntu@kgproject-4.openstacklocal>
2016-05-18 17:04:24 +0000

    now with textfile output

--- spark/seq.py
+++ spark/seq.py
@@ -1,5 +1,5 @@
 from pyspark import SparkContext
-sc = SparkContext("local", "App Name", pyFiles=['MyFile.py', 'lib.zip', 'app.egg'])
+sc = SparkContext()
 
 import pysam
 bamUrl = "http://130.238.29.253:8080/swift/v1/1000-genomes-dataset/HG00096.chrom20.ILLUMINA.bwa.GBR.low_coverage.20120522.bam"
@@ -12,7 +12,7 @@ with pysam.AlignmentFile(bamUrl,"rb") as samfile:
                         for x in range(len(test)+1 - kmer):
                                 kmers = test[x:x+kmer]
                                 kmer_list.append(kmers)
-                                #with open("/home/ubuntu/output/out_seq.txt", "a") as f:
-                                #       f.write(str(kmers) + "\n")
-                        print  len(kmer_list)
+                                with open("/home/ubuntu/genome_project/spark/output.txt", "a") as f:
+                                       f.write(str(kmers) + "\n")
+        print len(kmer_list)
         samfile.close()
