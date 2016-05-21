d0af54ef023a7efa2f19a882f6c8a05d4a945f6a
Maria Svensson <ubuntu@kgproject-4-large.openstacklocal>
2016-05-21 20:57:54 +0000

    trying to extract read position

--- spark/seq_version2.py
+++ spark/seq_version2.py
@@ -4,13 +4,20 @@ import pysam,os
 sc = SparkContext()
 PATH = "/home/ubuntu/genome_project/spark/bam_files/"
 KMER_PATH = "/home/ubuntu/genome_project/spark/txt_files/"
+HEAT = 	    "/home/ubuntu/genome_project/spark/heat/"
+KUL =       "/home/ubuntu/genome_project/spark"
+KMER =      "/home/ubuntu/genome_project/spark/kmer/"
 #Find all kmers and save in file
 def findKmers(file):
         samfile = pysam.AlignmentFile(PATH+file, "rb")
         kmer = 10
         kmer_list = []
+	heat_list = []
         for r in samfile.fetch(until_eof=True):
                 if r.is_unmapped:
+			position = read.next_reference_start
+			with open(HEAT + file[:7]+".txt", "a") as f:
+                                               f.write(str(position) + "\n")
                         test = r.query_alignment_sequence
                         for x in range(len(test)+1 - kmer):
                                 kmers = test[x:x+kmer]
@@ -30,11 +37,15 @@ def filterKmer(tuple):
 def bamFiles():
         bamUrl = os.listdir("/home/ubuntu/genome_project/spark/bam_files")
         #distFiles = sc.parallelize(bamUrl)
-        bamFiles = bamUrl[:6]
+        if not os.path.exists(KUL + "/heat/"):
+                os.makedirs(KUL + "/heat/")
+        if not os.path.exists(KUL + "/kmer/"):
+                os.makedirs(KUL + "/kmer/")
+	bamFiles = bamUrl[:2]
         distFiles = sc.parallelize(bamFiles)
 	
 	kmer_res = distFiles.flatMap(lambda file: findKmers(file)).map(lambda word: (word,1)).reduceByKey(lambda a,b: a+b)
-	kmer_res.saveAsTextFile("output_RAW")
+	#kmer_res.saveAsTextFile("output_RAW")
 
 	kmer_range = kmer_res.map(lambda line: filterKmer(line)).filter(lambda obj: obj != None)
 	kmer_range.saveAsTextFile("kmers_range")
