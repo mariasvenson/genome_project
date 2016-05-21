f0d25f6a607929f3e699c505b6913ed5d7c5c7e9
Maria Svensson <ubuntu@kgproject-4.openstacklocal>
2016-05-21 11:46:36 +0000

    new structure, new mapReduce

--- spark/seq_version3.py
+++ spark/seq_version3.py
@@ -1,13 +1,11 @@
-def bamFiles():
-        bamUrl = os.listdir("/home/ubuntu/genome_project/spark/bam_files")
-        #distFiles = sc.parallelize(bamUrl)
-        a = bamUrl[:2]
-        distFiles = sc.parallelize(a)
+from pyspark import SparkContext
+import pysam,os
 
-        result = distFiles.foreach(lambda file: runFile(file))
-        reduceKmers()
-
-def runFile(file):
+sc = SparkContext()
+PATH = "/home/ubuntu/genome_project/spark/bam_files/"
+KMER_PATH = "/home/ubuntu/genome_project/spark/txt_files/"
+#Find all kmers and save in file
+def findKmers(file):
         samfile = pysam.AlignmentFile(PATH+file, "rb")
         kmer = 10
         kmer_list = []
@@ -17,16 +15,32 @@ def runFile(file):
                         for x in range(len(test)+1 - kmer):
                                 kmers = test[x:x+kmer]
                                 kmer_list.append(kmers)
-                                with open("/home/ubuntu/genome_project/spark/txt_files/"+ file[:7]+".txt", "a") as f:
+                                with open(KMER_PATH+ file[:7]+".txt", "a") as f:
                                                 f.write(str(kmers) + "\n")
         print len(kmer_list)
+	samfile.close()
+	return kmer_list
 
-def reduceKmers():
-        text_file = sc.textFile("/home/ubuntu/genome_project/spark/txt_files/*")
-        counts = text_file.flatMap(lambda line: line.split("\n")) \
-                                .map(lambda word: (word, 1)) \
-                                .reduceByKey(lambda a, b: a + b)
-        counts.saveAsTextFile("/home/ubuntu/genome_project/spark/omgwow.txt")
-        samfile.close()
+def filterKmer(tuple):
+	if tuple[1] >= 10 and tuple[1] <= 200:
+		return tuple
+	else:
+		return None
 
+def bamFiles():
+        bamUrl = os.listdir("/home/ubuntu/genome_project/spark/bam_files")
+        #distFiles = sc.parallelize(bamUrl)
+        bamFiles = bamUrl[:2]
+        distFiles = sc.parallelize(bamFiles)
+	
+	kmer_res = distFiles.flatMap(lambda file: findKmers(file)).map(lambda word: (word,1)).reduceByKey(lambda a,b: a+b)
+	kmer_res.saveAsTextFile("output_RAW")
+
+	kmer_range = kmer_res.map(lambda line: filterKmer(line)).filter(lambda obj: obj != None)
+	kmer_range.saveAsTextFile("kmers_range")
+	for obj in kmer_range.collect():
+		with open("/home/ubuntu/kmers.txt/home/ubuntu/genome_project/spark/EVERYTHING.txt", "a") as f:
+			f.write(str(obj) + "\n")
+	
+    
 bamFiles()
